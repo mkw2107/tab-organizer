88c40afc87deacc96dfc85d3a6a2767d71409a45
akempel <anna.kb.kempel@gmail.com>
2024-09-15 21:06:59 -0400

    adds a switch button and correctly styles it below tab title list item

--- main.js
+++ main.js
@@ -5,35 +5,37 @@ document.addEventListener('DOMContentLoaded', () => {
   chrome.tabs.query({ windowId: chrome.windows.WINDOW_ID_CURRENT }, (tabs) => {
     console.log(tabs);
 
-    // iterate over array of tabs
-    // for each tab
-    // create new li item
-    // add innerhtml to li item equal to tab title
-    // create new button item with innerhtml of Delete Tab
-    // add onclick event listener to button with anonymous function invoking deleteTab with current tabId
-    // append button to li item
-    // append li to ol
     const tabListElement = document.getElementById('tab-list');
 
+    // add tab title to list, and add delete and switch buttons
     for (let i = 0; i < tabs.length; i++) {
       const listItem = document.createElement('li');
       listItem.innerHTML = `${tabs[i].title}`;
 
-      const button = document.createElement('button');
-      button.innerHTML = 'Delete Tab';
-      button.addEventListener('click', () => {
+      const buttonContainer = document.createElement('div');
+      buttonContainer.classList.add('button-container');
+
+      const deleteButton = document.createElement('button');
+      deleteButton.innerHTML = 'Delete Tab';
+      deleteButton.addEventListener('click', () => {
         deleteTab(tabs[i].id);
         window.location.reload();
       });
+      deleteButton.classList.add('delete-tab');
 
-      listItem.appendChild(button);
+      const switchButton = document.createElement('button');
+      switchButton.innerHTML = 'Switch to';
+      switchButton.classList.add('switch-tab');
+
+      listItem.appendChild(buttonContainer);
+      buttonContainer.appendChild(switchButton);
+      buttonContainer.appendChild(deleteButton);
       tabListElement.appendChild(listItem);
     }
 
+    // Close All Tabs
     let closeAllButton = document.getElementById('close-all');
     closeAllButton.addEventListener('click', () => {
-      // open new tab
-      // close all other tabs
       chrome.tabs.create({});
       for (let i = 0; i < tabs.length; i++) {
         deleteTab(tabs[i].id);
