fb1ae4fbf15f87d89c6073848dcad69ccc7b6325
akempel <anna.kb.kempel@gmail.com>
2024-09-16 00:46:21 -0400

    adds delete searched tabs function

--- main.js
+++ main.js
@@ -4,20 +4,24 @@ To Add:
 - delete all (by group, or just delete google searches)
 */
 
+// add delete button for search results
+// when search bar has anything other than an empty string, have a delete results button available
+// delete search results button should delete only the tabs that are in the list
+
 document.addEventListener('DOMContentLoaded', () => {
-  function deleteTab(tabId) {
-    chrome.tabs.remove(tabId);
-  }
-
-  function switchTab(tabId) {
-    chrome.tabs.update(tabId, { active: true });
-  }
-
   // query the current window's tabs, and do things with the tabs
   chrome.tabs.query({ windowId: chrome.windows.WINDOW_ID_CURRENT }, (tabs) => {
-    console.log(tabs);
+    function deleteTab(tabId) {
+      chrome.tabs.remove(tabId);
+    }
 
+    function switchTab(tabId) {
+      chrome.tabs.update(tabId, { active: true });
+    }
+
+    // variables
     const tabListElement = document.getElementById('tab-list');
+    let searchedTabs = [];
 
     // Creating/maintaing tab list
     function addTabToList(tab) {
@@ -55,6 +59,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function clearList() {
       tabListElement.innerHTML = '';
+      searchedTabs = [];
     }
 
     // add tab title to list, and add delete and switch buttons
@@ -87,6 +92,17 @@ document.addEventListener('DOMContentLoaded', () => {
     let closeAllButton = document.getElementById('close-all');
     closeAllButton.addEventListener('click', closeAll);
 
+    // Delete Searched Tabs
+    const deleteSearchButton = document.createElement('button');
+    deleteSearchButton.innerHTML = 'Delete Searched Tabs';
+    const searchContainer = document.getElementById('search-container');
+    function deleteSearchResults() {
+      for (let i = 0; i < searchedTabs.length; i++) {
+        deleteTab(searchedTabs[i].id);
+      }
+    }
+    deleteSearchButton.addEventListener('click', deleteSearchResults);
+
     // Search input
     function searchTabs(input) {
       clearList();
@@ -94,12 +110,19 @@ document.addEventListener('DOMContentLoaded', () => {
       for (let i = 0; i < tabs.length; i++) {
         if (tabs[i].url.includes(input) || tabs[i].title.includes(input)) {
           addTabToList(tabs[i]);
+          searchedTabs.push(tabs[i]);
+          console.log(searchedTabs);
         }
+      }
+
+      if (input.length > 0) {
+        searchContainer.appendChild(deleteSearchButton);
+      } else {
+        searchContainer.removeChild(deleteSearchButton);
       }
     }
     const searchInput = document.getElementById('search');
     searchInput.addEventListener('input', (e) => {
-      console.log('input received!');
       searchTabs(e.target.value);
     });
   });
